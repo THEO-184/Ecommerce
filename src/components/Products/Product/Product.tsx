da8cbdb6779ca4daea3db167b3294c4ef09bfff1
THEO-184 <theophilusboakye47@gmail.com>
2022-01-05 16:00:10 +0000

    added disable features to products

--- src/components/Products/Product/Product.tsx
+++ src/components/Products/Product/Product.tsx
@@ -4,19 +4,24 @@ import {
 	CardActions,
 	CardContent,
 	CardMedia,
+	Paper,
 	Typography,
 } from "@mui/material";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { ProductType } from "../../../types";
 import { Box } from "@mui/system";
 import { useAppSelector, useAppDispatch } from "../../../app/hooks";
-import { selectProducts } from "../../../features/Products/ProductsSlice";
-import { ADD_TO_CART } from "../../../features/CartSlice";
+import {
+	selectProducts,
+	DISABLE_BUTTON,
+} from "../../../features/Products/ProductsSlice";
+import { ADD_TO_CART, INCREASE_ITEM } from "../../../features/CartSlice";
+import { flex } from "./Product.styles";
+
 // ====COMPONENT =====
 const Product = (props: ProductType) => {
-	const { id, title, price, image, description } = props;
+	const { id, title, price, image, description, disabled } = props;
 	const PRODUCTS = useAppSelector(selectProducts);
-
 	const dispatch = useAppDispatch();
 
 	// Add To Cart
@@ -24,49 +29,57 @@ const Product = (props: ProductType) => {
 		PRODUCTS.map((product) => {
 			if (product.id === id) {
 				dispatch(ADD_TO_CART(product));
+				// dispatch(INCREASE_ITEM(id));
+				dispatch(DISABLE_BUTTON(id));
 			} else return;
 		});
 	};
 	return (
 		<Box>
-			<Card sx={{ maxWidth: "100%" }}>
-				<CardMedia component={"img"} height={"170"} alt="img" image={image} />
-				<CardContent>
-					<Box
-						sx={{
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "space-between",
-							py: 0.1,
-						}}
-					>
-						<Typography component="h4" variant="body2">
-							{title.length > 20 ? title.slice(0, 20) + "..." : title}
+			<Paper elevation={3}>
+				<Card sx={{ maxWidth: "100%" }}>
+					<CardMedia component={"img"} height={"170"} alt="img" image={image} />
+					<CardContent>
+						<Box
+							sx={{
+								...flex,
+								py: 0.1,
+							}}
+						>
+							<Typography component="h4" variant="body2">
+								{title.length > 20 ? title.slice(0, 20) + "..." : title}
+							</Typography>
+							<Typography component="h4" variant="body2">
+								${price}
+							</Typography>
+						</Box>
+						<Typography variant="body2" sx={{ mt: 1 }}>
+							{description
+								? description.length > 30
+									? description.slice(0, 30).toLowerCase() + "..."
+									: description
+								: "Product"}
 						</Typography>
-						<Typography component="h4" variant="body2">
-							${price}
-						</Typography>
-					</Box>
-					<Typography variant="body2" sx={{ mt: 1 }}>
-						{description
-							? description.length > 30
-								? description.slice(0, 30).toLowerCase() + "..."
-								: description
-							: "Product"}
-					</Typography>
-				</CardContent>
-				<CardActions>
-					<Box flexGrow={1} />
-					<Button
-						variant="text"
-						disableElevation
-						sx={{ "&:hover": { background: 0 } }}
-						onClick={() => handleAddToCart()}
-					>
-						<ShoppingCartOutlinedIcon sx={{ color: "#000" }} />
-					</Button>
-				</CardActions>
-			</Card>
+					</CardContent>
+					<CardActions>
+						<Box flexGrow={1} />
+						{disabled ? (
+							<Button variant="text" disabled>
+								in cart
+							</Button>
+						) : (
+							<Button
+								variant="text"
+								disableElevation
+								sx={{ "&:hover": { background: 0 } }}
+								onClick={() => handleAddToCart()}
+							>
+								<ShoppingCartOutlinedIcon sx={{ color: "#000" }} />
+							</Button>
+						)}
+					</CardActions>
+				</Card>
+			</Paper>
 		</Box>
 	);
 };
